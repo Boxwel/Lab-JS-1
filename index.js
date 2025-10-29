f5aa6cc0e66ce8495f6537a84b008ff930f64f2b
Boxwel <sramirezesc@gmail.com>
2025-10-29 14:41:39 -0500

    Ejercicios en proceso...

---
+++ index.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")();
+
+//! Ejercicio #1
+// Nombre
+let miNombre;
+miNombre = "Santiago"
+console.log(miNombre);
+// Edad
+let miEdad;
+miEdad = "23"
+console.log(miEdad);
+// esEstudiante
+let esEstudiante;
+if (true){
+    let esEstudiante = "Si";
+    console.log(esEstudiante);
+}
+
+//! Ejercicio #2
